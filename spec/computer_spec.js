89027162c13c4fa4133bff0e85aab8c1755398ca
Klint Tiley <kctiley@gmail.com>
2016-05-06 09:40:59 -0600

    Uncommented tests

--- spec/computer_spec.js
+++ spec/computer_spec.js
@@ -13,50 +13,50 @@ describe('Computer', function () {
     
   describe('methods', function () {
     describe('finds available side moves', function () {
-      // it('all sides are available initially', function () {
-      //   var board = new Board();
-      //   computer.move(board);
-      //   expect(computer.availableSides(board)).toEqual(['topCenter', 'middleRight', 'bottomCenter', 'middleLeft']);
-      // });
+      it('all sides are available initially', function () {
+        var board = new Board();
+        computer.move(board);
+        expect(computer.availableSides(board)).toEqual(['topCenter', 'middleRight', 'bottomCenter', 'middleLeft']);
+      });
 
-    //   it('determines only available side moves', function () {
-    //     var board = new Board();
-    //     computer.move(board);
-    //     board.setMarker('topCenter', x);
-    //     expect(computer.availableSides(board)).toEqual(['middleRight', 'bottomCenter', 'middleLeft']);
-    //   });
+      it('determines only available side moves', function () {
+        var board = new Board();
+        computer.move(board);
+        board.setMarker('topCenter', x);
+        expect(computer.availableSides(board)).toEqual(['middleRight', 'bottomCenter', 'middleLeft']);
+      });
 
-    // });
+    });
 
-    // describe('finds available corner move', function () {
-    //   it('all corners are available initially', function () {
-    //     var board = new Board();
-    //     computer.move(board);
-    //     expect(computer.availableCorners(board)).toEqual(['topLeft', 'topRight', 'bottomRight', 'bottomLeft']);
-    //   });
+    describe('finds available corner move', function () {
+      it('all corners are available initially', function () {
+        var board = new Board();
+        computer.move(board);
+        expect(computer.availableCorners(board)).toEqual(['topLeft', 'topRight', 'bottomRight', 'bottomLeft']);
+      });
 
-    //   it('finds only available corner moves', function () {
-    //     var board = new Board();
-    //     computer.move(board);
-    //     board.setMarker('topLeft', x);
-    //     expect(computer.availableCorners(board)).toEqual(['topRight', 'bottomRight', 'bottomLeft']);
-    //   });
+      it('finds only available corner moves', function () {
+        var board = new Board();
+        computer.move(board);
+        board.setMarker('topLeft', x);
+        expect(computer.availableCorners(board)).toEqual(['topRight', 'bottomRight', 'bottomLeft']);
+      });
 
-    // });
+    });
 
-    // describe('finds available center move', function () {
-    //   it('center available initially', function () {
-    //     var board = new Board();
-    //     computer.move(board);
-    //     expect(computer.availableCenter(board)).toEqual(['center']);
-    //   });
+    describe('finds available center move', function () {
+      it('center available initially', function () {
+        var board = new Board();
+        computer.move(board);
+        expect(computer.availableCenter(board)).toEqual(['center']);
+      });
 
-    //   it('finds only available center move', function () {
-    //     var board = new Board();
-    //     computer.move(board);
-    //     board.setMarker('center', x);
-    //     expect(computer.availableCenter(board)).toEqual([]);
-    //   });
+      it('finds only available center move', function () {
+        var board = new Board();
+        computer.move(board);
+        board.setMarker('center', x);
+        expect(computer.availableCenter(board)).toEqual([]);
+      });
 
     });
 
